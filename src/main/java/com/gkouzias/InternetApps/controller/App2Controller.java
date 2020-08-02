b43b87348e344b85c010e61eb7e8f3bbb0a09e88
Alkis <gk.alkiviadis@gmail.com>
2020-08-03 00:56:08 +0300

    implemented scheduled fetch on weather database

--- src/main/java/com/gkouzias/InternetApps/controller/App2Controller.java
+++ src/main/java/com/gkouzias/InternetApps/controller/App2Controller.java
@@ -8,6 +8,8 @@ import com.gkouzias.InternetApps.model.App2DTO;
 import com.gkouzias.InternetApps.service.PathsService;
 import com.gkouzias.InternetApps.service.StopsService;
 import com.gkouzias.InternetApps.service.WeatherConditionsService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +35,6 @@ public class App2Controller {
     @GetMapping("")
     ResponseEntity<?> viewStopsController(){
         List<Path> paths = pathsService.findAll();
-        scheduleFixedDelayTask();
         if(paths.isEmpty()) return new ResponseEntity<>(ResponseEntity.notFound().build(), HttpStatus.NOT_FOUND);
         else {
             List<App2DTO> app2DTOs = new ArrayList<>();
@@ -49,6 +51,35 @@ public class App2Controller {
     @Autowired
     private StopsService stopsService;
 
+
+    // call weather api each 5 minutes to update our db with weather conditions
+    @Scheduled(fixedDelay = 5*60*1000)
+    public void weatherUpdate() {
+        System.out.println("start");
+        RestTemplate restTemplate = new RestTemplate();
+        List<Stop> stops = stopsService.findAll();
+        for(Stop stop:stops){
+            // for each stop do a request using lat and lon to get it's weather info
+            final String nameURI = "http://api.openweathermap.org/data/2.5/weather?lat="+stop.getLat()+"&lon="+stop.getLon()+"&APPID="+api_key;
+            String res = restTemplate.getForObject(nameURI, String.class);
+            JsonObject jsonObject = new JsonParser().parse(res).getAsJsonObject();
+
+            // get main and desc
+            String weather_main = jsonObject.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("main").toString();
+            String weather_desc = jsonObject.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("description").toString();
+            weather_main = weather_main.replace("\"",""); weather_main = weather_main.replace("\"","");
+            weather_desc = weather_desc.replace("\"", ""); weather_desc = weather_desc.replace("\"", "");
+
+            // update each stop weather condition
+            WeatherCondition wc = weatherConditionsService.findByStopWc(stop);
+            wc.setWeather_desc(weather_desc);
+            wc.setWeather_main(weather_main);
+            weatherConditionsService.save(wc);
+        }
+        System.out.println("stop");
+    }
+
+
     @CrossOrigin(origins = "http://localhost:8081")     // allow remote access from http://localhost:8081 [CORS]
     @GetMapping("/test")
     ResponseEntity<?> test(){
@@ -60,13 +91,5 @@ public class App2Controller {
         return new ResponseEntity<>("a", HttpStatus.OK);
     }
 
-
-
-    // call weather api each 5 minutes to update our db with weather conditions
-    @Scheduled(fixedDelay = 5*60*1000)
-    public void scheduleFixedDelayTask() {
-        System.out.println(
-                "Fixed delay task - " + System.currentTimeMillis() / 1000);
-    }
 
 }
