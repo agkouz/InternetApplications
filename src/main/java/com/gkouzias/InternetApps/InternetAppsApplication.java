3fc7d6f3caf899eddb661ebfa73cc23cecc5f74d
Alkis <gk.alkiviadis@gmail.com>
2020-08-02 19:45:54 +0300

    Implemented repo for WeatherConditions

--- src/main/java/com/gkouzias/InternetApps/InternetAppsApplication.java
+++ src/main/java/com/gkouzias/InternetApps/InternetAppsApplication.java
@@ -2,8 +2,10 @@ package com.gkouzias.InternetApps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class InternetAppsApplication {
 
 	public static void main(String[] args) {
