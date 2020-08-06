0ee91cce422995adba513d1c6b288fe89516314d
Alkis <gk.alkiviadis@gmail.com>
2020-08-06 16:06:47 +0300

    Added /api/app3/getData/ endpoint

--- src/main/java/com/gkouzias/InternetApps/controller/WeatherStatisticsController.java
+++ src/main/java/com/gkouzias/InternetApps/controller/WeatherStatisticsController.java
@@ -1,6 +1,7 @@
 package com.gkouzias.InternetApps.controller;
 
 
+import com.gkouzias.InternetApps.domain.DayWeather;
 import com.gkouzias.InternetApps.domain.Transport;
 import com.gkouzias.InternetApps.model.DayWeatherDTO;
 import com.gkouzias.InternetApps.service.DayWeatherService;
@@ -31,17 +32,26 @@ public class WeatherStatisticsController {
 
 
     @CrossOrigin(origins = "http://localhost:8081")     // allow remote access from http://localhost:8081 [CORS]
+    @GetMapping("/getData/")
+    ResponseEntity<?> getAllTransportsDataController() {
+
+        List<DayWeather> dayWeathers = dayWeatherService.findAll();
+
+        // populate if empty using external api
+        if(dayWeathers.isEmpty()) {
+            List<Transport> transports = transportService.findAll();
+            if(transports.isEmpty()) return new ResponseEntity<>(ResponseEntity.notFound().build(), HttpStatus.NOT_FOUND);
+            dayWeatherService.populateDayWeather(transports);
+        }
+        return new ResponseEntity<>(dayWeathers.stream().map(dw -> new DayWeatherDTO(dw.getWeatherClass(), (int) dw.getTotalUsers(), dw.getEventDate())), HttpStatus.OK);
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:8081")     // allow remote access from http://localhost:8081 [CORS]
     @GetMapping("/getData/{month}")
     ResponseEntity<?> getTransportsDataController(@PathVariable String month ){
         if(!month.equals("08") && !month.equals("11")) return new ResponseEntity<>(ResponseEntity.badRequest().build(), HttpStatus.BAD_REQUEST);
         else {
-            // if day_weather is empty, we need to populate
-            if(dayWeatherService.count() == 0){
-                List<Transport> transports = transportService.findAll();
-                if(transports.isEmpty()) return new ResponseEntity<>(ResponseEntity.notFound().build(), HttpStatus.NOT_FOUND);
-                dayWeatherService.populateDayWeather(transports);
-            }
-
             // if day_weather is populated, find max transports accordingly
             String month1 = "____" + month + "__";
             String month2 = "____" + String.valueOf(Integer.parseInt(month) + 1) + "__";
